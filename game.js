79be2c79cc8b5f956d484aa120eec6da6e18a639
thounny <thounnykeo@gmail.com>
2021-11-21 00:09:29 -0500

    added audio to dedicated color buttons

--- game.js
+++ game.js
@@ -1,12 +1,29 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+
+});
+
 function nextSequence() {
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    playSound(randomChosenColor);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
